5d24757b3b4d6957a99f0bc3de07cd578c8a441d
pjoshk1-dev <pjoshk1@gmail.com>
2025-11-27 12:02:27 -0700

    modified backend index.js

---
+++ service/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// API Routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+// Serve frontend using Express static middleware
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Backend running at http://localhost:${PORT}`);
+});
